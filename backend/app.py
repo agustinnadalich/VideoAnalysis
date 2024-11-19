8091c2ab972551b88b7c6c9a44048cb0ccc9fd58
agustinnadalich <agustin.nadalich@gmail.com>
2024-11-19 02:14:56 +0100

    timeline

--- backend/app.py
+++ backend/app.py
@@ -13,16 +13,16 @@ if not os.path.exists(UPLOAD_FOLDER):
 @app.route('/events', methods=['GET'])
 def get_events():
     events = [
-        {"type": "Tackle", "descriptor": "Player 1", "duration": 3, "time": 45, "result": "Success"},
-        {"type": "Tackle", "descriptor": "Player 2", "duration": 3, "time": 12, "result": "Failure"},
-        {"type": "Tackle", "descriptor": "Player 3", "duration": 3, "time": 30, "result": "Success"},
-        {"type": "Tackle", "descriptor": "Player 4", "duration": 3, "time": 22, "result": "Failure"},
-        {"type": "Tackle", "descriptor": "Player 5", "duration": 3, "time": 55, "result": "Success"},
-        {"type": "Tackle", "descriptor": "Player 3", "duration": 3, "time": 18, "result": "Failure"},
-        {"type": "Tackle", "descriptor": "Player 2", "duration": 3, "time": 37, "result": "Success"},
-        {"type": "Tackle", "descriptor": "Player 3", "duration": 3, "time": 29, "result": "Success"},
-        {"type": "Tackle", "descriptor": "Player 4", "duration": 3, "time": 41, "result": "Success"},
-        {"type": "Tackle", "descriptor": "Player 5", "duration": 3, "time": 50, "result": "Success"},
+        {"id": 1, "type": "Tackle", "descriptor": "Player 2", "duration": 3, "time": 12, "result": "Failure"},
+        {"id": 2, "type": "Tackle", "descriptor": "Player 3", "duration": 3, "time": 18, "result": "Failure"},
+        {"id": 3, "type": "Tackle", "descriptor": "Player 4", "duration": 3, "time": 22, "result": "Failure"},
+        {"id": 4, "type": "Tackle", "descriptor": "Player 3", "duration": 3, "time": 29, "result": "Success"},
+        {"id": 5, "type": "Tackle", "descriptor": "Player 3", "duration": 3, "time": 30, "result": "Success"},
+        {"id": 6, "type": "Tackle", "descriptor": "Player 2", "duration": 3, "time": 37, "result": "Success"},
+        {"id": 7, "type": "Tackle", "descriptor": "Player 4", "duration": 3, "time": 41, "result": "Success"},
+        {"id": 8, "type": "Ruck", "descriptor": "Player 1", "duration": 3, "time": 45, "result": "Success"},
+        {"id": 9, "type": "Ruck", "descriptor": "Player 5", "duration": 3, "time": 50, "result": "Success"},
+        {"id": 10, "type": "Tackle", "descriptor": "Player 5", "duration": 3, "time": 55, "result": "Success"}
     ]
     print(events)  # Verifica los datos en la consola del servidor
     return jsonify(events)
